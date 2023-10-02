8cf5ff52a1aed398ca95abd3fb5c80d7ae7588b6
akprojs-me <144190667+akprojs-me@users.noreply.github.com>
2023-10-02 10:22:52 -0400

    feat: add play and stop buttons

--- pendulum.js
+++ pendulum.js
@@ -13,9 +13,52 @@ sliders.forEach((slider, index) => {
 const canvas = document.getElementById('pendulum-canvas');
 const ctx = canvas.getContext('2d');
 
-// Function to set canvas size based on parent container size
-function setCanvasSize() {
-    const container = document.getElementById('canvas-container');
-    canvas.width = container.clientWidth;
-    canvas.height = container.clientHeight;
+// Draw out the initial state of the pendulum
+function updatePendulum() {
+    let rodLength = document.getElementById('length_input').value;
+    let mass = document.getElementById('mass_input').value;
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Play and Stop Buttons
+    let buttonsWidth = 200;
+    let buttonsHeight = 100;
+    let startingX = canvas.width / 2 - buttonsWidth / 2;
+
+    let padButtons = 20;
+    let startingY = canvas.height - buttonsHeight - padButtons;
+    let buttonHeight = buttonsHeight - 2 * padButtons;
+
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(startingX, startingY, buttonsWidth, buttonsHeight);
+    console.log("startingX: ", startingX);
+    console.log("startingY: ", startingY);
+    console.log("canvas width: ", canvas.width)
+    console.log("canvas height: ", canvas.height)
+
+    ctx.strokeStyle = "#297511";
+    ctx.beginPath();
+    ctx.moveTo(startingX + padButtons, startingY + padButtons);
+    ctx.lineTo(startingX + padButtons, startingY + padButtons + buttonHeight);
+    ctx.lineTo(startingX + padButtons + buttonHeight / 2.0, startingY + padButtons + buttonHeight / 2.0);
+    console.log("triangle point 1: ", startingX + padButtons, startingY + padButtons);
+    console.log("triangle point 2: ", startingX + padButtons, startingY + padButtons + buttonHeight);
+    console.log("triangle point 3: ", startingX + padButtons + buttonHeight / 2.0, startingY + padButtons + buttonHeight / 2.0);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fillStyle = "#92D050";
+    ctx.fill();
+
+    ctx.fillStyle = "#8C8303";
+    ctx.fillRect(startingX + buttonsWidth / 2 + padButtons, startingY + padButtons, buttonHeight, buttonHeight);
+
+
+
+
+
+
+
 }
+
+updatePendulum()
+
